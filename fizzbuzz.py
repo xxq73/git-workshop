6a21bc56a501ec221a66065f4de16e9f49b766c7
huangy6 <huangy6@yahoo.com>
2018-01-19 00:47:56 -0500

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def fizzbuzz(n):
+    for i in range(n):
+        if i % 15 == 0:
+            print "fizzbuzz"
+        elif i % 3 == 0:
+            print "fizz"
+        elif i % 5 == 0:
+            print "buzz"
+        else:
+            print i
