dddf44d7ef7ffcccd45843499a07fc5e679f502e
huangy6 <huangy6@yahoo.com>
2018-01-20 22:47:31 -0500

    Refactoring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
 def fizzbuzz(n):
-    for i in range(n):
-        if i % 30 == 0:
-            print "fizzbuzzboom"
-        elif i % 15 == 0:
-            print "fizzbuzz"
-        elif i % 3 == 0:
-            print "fizz"
-        elif i % 10 == 0:
-            print "buzzboom"
-        elif i % 6 == 0:
-            print "fizzboom"
-        elif i % 5 == 0:
-            print "buzz"
-        elif i % 2 == 0:
-            print "boom"
-        else:
-            print i
+    for i in range(1, n + 1):
+        s = ""
+        if i % 3 == 0:
+            s += "fizz"
+        if i % 5 == 0:
+            s += "buzz"
+        if i % 7 == 0:
+            s += "bizz"
+        if not s:
+            s = "%d" % i
+        print s
