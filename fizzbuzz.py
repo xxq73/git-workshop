f518edc55cb399f434226991dbcd7a19dd431131
huangy6 <huangy6@yahoo.com>
2018-01-20 21:59:57 -0500

    Print boom on multiples of 2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,19 @@
 
 def fizzbuzz(n):
     for i in range(n):
-        if i % 15 == 0:
+        if i % 30 == 0:
+            print "fizzbuzzboom"
+        elif i % 15 == 0:
             print "fizzbuzz"
         elif i % 3 == 0:
             print "fizz"
+        elif i % 10 == 0:
+            print "buzzboom"
+        elif i % 6 == 0:
+            print "fizzboom"
         elif i % 5 == 0:
             print "buzz"
+        elif i % 2 == 0:
+            print "boom"
         else:
             print i
